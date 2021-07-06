720c06cef518e345f1ab3612e06a05896fc03f43
Abhishek Mishra <mail4abhishek.94@gmail.com>
2021-07-06 18:57:26 +0530

    commit

--- src/app/model/mou.model.ts
+++ src/app/model/mou.model.ts
@@ -1,24 +1,29 @@
-export class mouModel{
-    public  refid :string;
-    public  int_Uni_Name :string;
-    public  dept_Name :string;
+export class mouModel {
+  public refid: string;
+  public int_Uni_Name: string;
+  public dept_Name: string;
 
-    public  address :string;
+  public address: string;
 
-    public  phoneNo :string;
-    public  email :string;
-    public  gstNo :string;
-    public  dir_Head_Name :string;
+  public phoneNo: string;
+  public email: string;
+  public gstNo: string;
+  public dir_Head_Name: string;
 
-    public  dir_Phone_No :string;
-    public  dir_Mobile_No:string;
-    public  dir_Email:string;
-    public  nodal_Name :string;
-    public  nodal_Phone_No:string;
+  public dir_Phone_No: string;
+  public dir_Mobile_No: string;
+  public dir_Email: string;
+  public nodal_Name: string;
+  public nodal_Phone_No: string;
 
-    public  nodal_Mobile_No :string;
-    public  nodal_Email :string;
-    public  nodal_Designation :string;
+  public nodal_Mobile_No: string;
+  public nodal_Email: string;
+  public nodal_Designation: string;
+  public dir_Designation: string;
 
-    public app_Status:string;
+  public int_Uni_Drop: string;
+  public dir_Designation_Drop: string;
+  public nodal_Designation_Drop: string;
+
+  public app_Status: string;
 } 
